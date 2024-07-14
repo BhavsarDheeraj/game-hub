ff6af6871daf9f094d2227cc4ccb33d0c8abb811
bhavsardheeraj <dheerajbhavsar940@gmail.com>
2024-07-14 22:05:05 +0530

    filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,13 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
-import useGenres from '../hooks/useGenres';
+import { Button, HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import useGenres, { Genre } from '../hooks/useGenres';
 import getCropperImageUrl from '../services/image-url';
 import GenreListSkeleton from './GenreListSkeleton';
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -14,14 +18,16 @@ const GenreList = () => {
     <List>
       {data.map(genre => (
         <ListItem key={genre.id}>
-          <HStack spacing={2} paddingY="5px">
-            <Image
-              boxSize="32px"
-              borderRadius="8px"
-              src={getCropperImageUrl(genre.image_background)}
-            />
-            <Text fontSize="lg">{genre.name}</Text>
-          </HStack>
+          <Button variant="link" onClick={() => onSelectGenre(genre)}>
+            <HStack spacing={2} paddingY="5px">
+              <Image
+                boxSize="32px"
+                borderRadius="8px"
+                src={getCropperImageUrl(genre.image_background)}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </Button>
         </ListItem>
       ))}
     </List>
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,27 +1,30 @@
 import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
-import { CanceledError } from 'axios';
-
-export interface Genre {
-  id: number;
-  name: string;
-}
+import { AxiosRequestConfig, CanceledError } from 'axios';
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  dependencies?: any[]
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState('');
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    setData([]);
     const controller = new AbortController();
     setLoading(true);
     apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .get<FetchResponse<T>>(endpoint, {
+        signal: controller.signal,
+        ...requestConfig,
+      })
       .then(res => setData(res.data.results))
       .catch(err => {
         if (err instanceof CanceledError) return;
@@ -30,7 +33,7 @@ const useData = <T>(endpoint: string) => {
       .finally(() => setLoading(false));
 
     return () => controller.abort();
-  }, []);
+  }, dependencies ?? []);
 
   return { data, error, isLoading };
 };
