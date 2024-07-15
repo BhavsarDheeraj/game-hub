7f3d203e979e4866648c42d9ab6ffcd582e04b09
bhavsardheeraj <dheerajbhavsar940@gmail.com>
2024-07-15 20:04:23 +0530

    search input ui

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,17 +1,24 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { IconButton, useColorMode } from '@chakra-ui/react';
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
 
 const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <HStack>
-      <Switch
-        colorScheme="green"
-        isChecked={colorMode === "dark"}
-        onChange={toggleColorMode}
-      />
-      <Text>Dark Mode</Text>
-    </HStack>
+    <IconButton
+      aria-label="Switch Color Mode"
+      icon={
+        colorMode === 'dark' ? (
+          <BsSunFill color="yellow" />
+        ) : (
+          <BsMoonFill color="black" />
+        )
+      }
+      onClick={toggleColorMode}
+      marginLeft={2}
+      borderRadius={20}
+      // colorScheme={colorMode === 'dark' ? 'yellow' : 'gray'}
+    />
   );
 };
 
